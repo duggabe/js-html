0fabf3cdcc43c92b02d7556d9de76f8b8ba0d459
duggabe <barry@dcsmail.net>
2021-07-15 07:39:14 -0500

    revise icon scaling

--- sponsors.js
+++ sponsors.js
@@ -10,8 +10,10 @@ myObj = {
     {"name":"DIAMOND", "entries":[
 
         /* NI / Ettus Research */
-        {"icon": "https://events.gnuradio.org/event/8/images/12-ettus-logo.png",
-        "url": "https://www.ettus.com/",
+        {"icon":"https://events.gnuradio.org/event/8/images/12-ettus-logo.png",
+        "icon_w":280,
+        "icon_h":103,
+        "url":"https://www.ettus.com/",
         "blurb":"Ettus Research, an NI brand, is the world’s leading supplier of software-defined radio platforms, combining ease of use and a robust open-source software community. From wireless research to deployment, we empower you with the SDR tools, systems, and support you need to bring the next generation of wireless technology to life. It’s time. Let’s Engineer Ambitiously™."}
 
         ]},
@@ -19,33 +21,58 @@ myObj = {
     {"name":"PLATINUM", "entries":[
 
         /* Analog Devices */
-        {"icon": "https://www.gnuradio.org/grcon/grcon20/sponsors/Analog_Devices_Logo.svg",
-        "url": "https://www.analog.com/",
+        {"icon":"https://www.gnuradio.org/grcon/grcon20/sponsors/Analog_Devices_Logo.svg",
+        "icon_w":236,
+        "icon_h":95,
+        "url":"https://www.analog.com/",
         "blurb":"Analog Devices Analog Devices (Nasdaq: ADI) is the leading global high-performance analog technology company dedicated to solving the toughest engineering challenges. We enable our customers to interpret the world around us by intelligently bridging the physical and digital with unmatched technologies that sense, measure, power, connect and interpret."},
 
         /* OpenCPI */
-        {"icon": "https://events.gnuradio.org/event/8/images/13-opencpi2.png",
-        "url": "https://www.opencpi.org/",
-        "blurb":"Open Component Portability Infrastructure (OpenCPI) is an open source software (OSS) framework for developing and executing component-based applications on heterogeneous embedded systems. As a framework for development and execution, OpenCPI supports defining, implementing, building and testing components, as well as executing applications based on those components in the targeted embedded systems.  By targeting heterogeneous systems, the framework supports development and execution across diverse processing technologies including GPPs (general purpose processors), FPGA (field programmable gate arrays), and GPUs (graphics processing units) assembled into mixed systems."}
+        {"icon":"https://events.gnuradio.org/event/8/images/13-opencpi2.png",
+        "icon_w":400,
+        "icon_h":200,
+        "url":"https://www.opencpi.org/",
+        "blurb":"Open Component Portability Infrastructure (OpenCPI) is an open source software (OSS) framework for developing and executing component-based applications on heterogeneous embedded systems. As a framework for development and execution, OpenCPI supports defining, implementing, building and testing components, as well as executing applications based on those components in the targeted embedded systems.  By targeting heterogeneous systems, the framework supports development and execution across diverse processing technologies including GPPs (general purpose processors), FPGA (field programmable gate arrays), and GPUs (graphics processing units) assembled into mixed systems."},
+
+        /* Epiq Solutions */
+        {"icon": "https://www.gnuradio.org/grcon/grcon20/sponsors/epiq.png",
+        "icon_w":862,
+        "icon_h":564,
+        "url":"https://epiqsolutions.com/",
+        "blurb":"Epiq Solutions develops cutting edge RF tools that provide situational awareness and detailed insight into RF environments to identify and take action against wireless threats. With more than a decade serving government-focused industries, Epiq Solutions understands how important speed, cost, and performance are for defense and security applications. Our radically small, state-of-the-art SDR transceiver modules and turnkey RF sensing tools lead the way in size, weight, and low-power consumption. Whether the need is to develop mission-critical defense communications for the battlefield or to protect sensitive information, Epiq Solutions is the trusted RF solution provider."}
+
 
         ]},
 
     {"name":"GOLD", "entries":[
 
         /* Peraton Labs */
-        {"icon": "https://events.gnuradio.org/event/8/images/11-Peraton%20Labs%20logo-trimmed.png",
-        "url": "https://www.peratonlabs.com/",
+        {"icon":"https://events.gnuradio.org/event/8/images/11-Peraton%20Labs%20logo-trimmed.png",
+        "icon_w":1236,
+        "icon_h":619,
+        "url":"https://www.peratonlabs.com/",
         "blurb":"What matters to our customers, matters to us. ​We create new technologies, advance emerging technologies and deliver innovative solutions that work at scale and in complex, real-world environments—making an impact in defense, communications, energy, transportation, finance and life sciences.​"},
 
         /* BlackLynx */
-        {"icon": "https://events.gnuradio.org/event/8/images/14-Black-Lynx-RGB-horizotal.png",
-        "url": "https://www.blacklynx.tech/",
+        {"icon": "https://events.gnuradio.org/event/8/images/17-blacklynx_rect.png",
+        "icon_w":1007,
+        "icon_h":748,
+        "url":"https://www.blacklynx.tech/",
         "blurb":"BlackLynx has focused primarily on providing mission critical edge compute for national security customers. Whether processing data on board a platform, the data center, or cloud, BlackLynx delivers the optimal solution. The BlackLynx team consists of highly technical programmers and device experts capable of solving the most difficult mission challenges."},
 
         /* Xilinx */
-        {"icon": "https://www.gnuradio.org/grcon/grcon20/sponsors/xilinx.png",
-        "url": "https://www.xilinx.com/",
-        "blurb":"Xilinx is the inventor of the FPGA, programmable SoCs, and now, the Adaptive Compute Acceleration Platform (ACAP). Our highly-flexible programmable silicon, enabled by a suite of advanced software and tools, enables rapid innovation across a wide span of industries and technologies, including high performance Software Defined Radio applications (from Direct RF integration to real-time Signal Processing and Machine Learning)."}
+        {"icon":"https://www.gnuradio.org/grcon/grcon20/sponsors/xilinx.png",
+        "icon_w":1750,
+        "icon_h":355,
+        "url":"https://www.xilinx.com/",
+        "blurb":"Xilinx is the inventor of the FPGA, programmable SoCs, and now, the Adaptive Compute Acceleration Platform (ACAP). Our highly-flexible programmable silicon, enabled by a suite of advanced software and tools, enables rapid innovation across a wide span of industries and technologies, including high performance Software Defined Radio applications (from Direct RF integration to real-time Signal Processing and Machine Learning)."},
+
+        /* Microsoft */
+        {"icon":"https://events.gnuradio.org/event/8/images/16-Microsoft.png",
+        "icon_w":216,
+        "icon_h":46,
+        "url":"https://www.microsoft.com/",
+        "blurb":"Our mission is to empower every person and every organization on the planet to achieve more."}
 
         ]},
 
@@ -55,10 +82,19 @@ myObj = {
 
     {"name":"PATRON", "entries":[
 
+        /* SkySafe */
+        {"icon":"https://www.gnuradio.org/grcon/grcon19/sponsors/skysafe.svg",
+        "icon_w":766,
+        "icon_h":766,
+        "url":"https://www.skysafe.io/",
+        "blurb":"SkySafe provides world-class drone defense and airspace control solutions. Founded in 2015 in San Diego, CA, our team consists of drone experts from MIT, UC San Diego, the Air Force Research Lab, and more. Our industry and intellectual experience includes leaders from the fields of security research, RF engineering, academia, and the military. We are a collection of diverse, capable, and hard working professionals who enjoy attacking the hardest challenges–we also have a lot of fun."},
+
         /* Duggan Computer Systems */
         {"icon": "https://events.gnuradio.org/event/8/images/6-DCS_logo.png",
+        "icon_w":308,
+        "icon_h":144,
         "url":"https://github.com/duggabe",
-        "blurb":"Duggan Computer Systems is a software consulting firm specializing in email security. Barry Duggan also is involved in the GNU Radio project, and is currently the Documentation Lead."}
+        "blurb": "Duggan Computer Systems is a software consulting firm specializing in email security. Barry Duggan also is involved in the GNU Radio project, and is currently the Documentation Lead."}
 
         ]}
   ]
@@ -95,7 +131,7 @@ if ((pos1 > 0) || (pos2 > 0))
         if (_lel > 0)
             {
             var t_th = document.createElement("TH");
-            t_th.setAttribute("class", "u_tr");         // yes, this is intentional
+            t_th.setAttribute("class", "u_th");
             if (_lel > 1)
                 t_th.setAttribute("colspan", _lel);
             /* create data row */
@@ -114,9 +150,12 @@ if ((pos1 > 0) || (pos2 > 0))
             {
             /* create data element */
             var t_td = document.createElement("TD");
-            t_td.setAttribute("class", "u_tr");         // yes, this is intentional
+            t_td.setAttribute("class", "u_td");
+            var aspect = (myObj.levels[i].entries[j].icon_w / myObj.levels[i].entries[j].icon_h);
+            var _td_h = 88 / aspect;
             var anchor = "<a href=\"" + myObj.levels[i].entries[j].url + "\">" +
-                "<img src=\"" + myObj.levels[i].entries[j].icon  + "\" style=\"width:75px;height:37px;\"></a>";
+                "<img src=\"" + myObj.levels[i].entries[j].icon  +
+                "\" style=\"width:88px;height:" + _td_h + "px;\"></a>";
             // console.log (anchor);
             t_td.innerHTML = anchor;
             t_tr.appendChild(t_td);
@@ -157,8 +196,12 @@ if ((pos1 > 0) || (pos2 > 0))
                 /* create data element */
                 var t_td = document.createElement("TD");    // icon
                 t_td.setAttribute("class", "u_tdl");
+                var aspect = (myObj.levels[i].entries[j].icon_w / myObj.levels[i].entries[j].icon_h);
+                var _td_h = 200 / aspect;
                 var anchor = "<a href=\"" + myObj.levels[i].entries[j].url + "\">" +
-                    "<img src=\"" + myObj.levels[i].entries[j].icon  + "\" style=\"max-width:200px;max-height:100px;\"></a>";
+                "<img src=\"" + myObj.levels[i].entries[j].icon  +
+                "\" style=\"width:200px;height:" + _td_h + "px;\"></a>";
+                // console.log (anchor);
                 t_td.innerHTML = anchor;
                 t_tr.appendChild(t_td);
                 var t_td = document.createElement("TD");    // blurb
